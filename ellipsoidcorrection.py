198362525ef971340119aa85f68d1fb92574573e
Matthew Bowden <bowdenm@spu.edu>
2018-07-06 15:04:49 -0700

    [WIP] Add percentile bounds and log adjustment

--- ellipsoidcorrection.py
+++ ellipsoidcorrection.py
@@ -18,7 +18,9 @@ NO           YES          NO
 
 import numpy
 import skimage
+import skimage.io
 import skimage.filters
+import skimage.exposure
 
 import cellprofiler.image
 import cellprofiler.module
@@ -53,17 +55,50 @@ class EllipsoidCorrection(cellprofiler.module.ImageProcessing):
             minval=0
         )
 
+        self.upper_percentile = cellprofiler.setting.Float(
+            text="Upper percentile intensity value cutoff",
+            value=99.,
+            minval=0.,
+            maxval=100.
+        )
+
+        self.set_percentile = cellprofiler.setting.Float(
+            text="Percentile value to set pixels beyond upper bound",
+            value=50.,
+            minval=0.,
+            maxval=100.
+        )
+
+        self.log_gain = cellprofiler.setting.Float(
+            text="Log adjustment gain",
+            value=1,
+        )
+
     def settings(self):
         __settings__ = super(EllipsoidCorrection, self).settings()
 
-        __settings__ += [self.sigma_z, self.sigma_y, self.sigma_x]
+        __settings__ += [
+            self.sigma_z,
+            self.sigma_y,
+            self.sigma_x,
+            self.upper_percentile,
+            self.set_percentile,
+            self.log_gain
+        ]
 
         return __settings__
 
     def visible_settings(self):
         __settings__ = super(EllipsoidCorrection, self).visible_settings()
 
-        __settings__ += [self.sigma_z, self.sigma_y, self.sigma_x]
+        __settings__ += [
+            self.sigma_z,
+            self.sigma_y,
+            self.sigma_x,
+            self.upper_percentile,
+            self.set_percentile,
+            self.log_gain
+        ]
 
         return __settings__
 
@@ -73,11 +108,24 @@ class EllipsoidCorrection(cellprofiler.module.ImageProcessing):
         super(EllipsoidCorrection, self).run(workspace)
 
 
-def ellipsoid_correct(image, sig_z, sig_y, sig_x):
+def ellipsoid_correct(image, sig_z, sig_y, sig_x, upper_percentile, set_percentile, log_gain):
     sigmas = (sig_z, sig_y, sig_x)
     super_smoothed = (skimage.filters.gaussian(image, sigma=sigmas, mode='constant') /
                       skimage.filters.gaussian(numpy.ones(image.shape), sigma=sigmas, mode='constant'))
+    skimage.util.io
 
-    corrected = skimage.img_as_float(image) - super_smoothed
+    # This is doing the opposite of what I want
+    corrected = skimage.img_as_float(image) * super_smoothed
 
-    return skimage.img_as_uint(corrected)
+    # Ensure all the values are positive
+    trimmed = corrected + numpy.abs(corrected.min())
+    upper = numpy.percentile(trimmed, upper_percentile)
+    set_val = numpy.percentile(trimmed, set_percentile)
+    trimmed[trimmed > upper] = set_val
+
+    # Log scale the image
+    # scaled = skimage.exposure.adjust_log(trimmed, gain=log_gain)
+
+    # scaled = skimage.exposure.rescale_intensity(scaled)
+
+    return skimage.img_as_uint(trimmed)
